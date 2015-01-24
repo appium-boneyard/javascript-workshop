75ca94023049de96a8d1ff389d51ca4d4e079dcc
Isaac Murchie <isaac@saucelabs.com>
2015-01-23 16:18:54 -0800

    Clean up apps and caps

--- tests/functional/helpers/apps.js
+++ tests/functional/helpers/apps.js
@@ -1,24 +1,19 @@
 'use strict';
 
+var path = require('path');
+
 if (process.env.SAUCE) {
   module.exports = {
-    iosTestApp: "https://appium.s3.amazonaws.com/TestApp7.1.app.zip",
-    iosWebviewApp: "http://appium.github.io/appium/assets/WebViewApp7.1.app.zip",
-    iosUICatalogApp: "http://appium.github.io/appium/assets/UICatalog7.1.app.zip",
-    androidApiDemos: "http://appium.github.io/appium/assets/ApiDemos-debug.apk",
-    selendroidTestApp: "http://appium.github.io/appium/assets/selendroid-test-app-0.10.0.apk",
+    iosTestApp: 'https://appium.s3.amazonaws.com/TestApp7.1.app.zip',
+    androidContactManager: 'https://appium.s3.amazonaws.com/ContactManager.apk',
     iosGappiumApp: 'https://appium.s3.amazonaws.com/HelloGappium.app.zip',
-
-    iosWebviewAppLocal: "http://localhost:3000/WebViewApp7.1.app.zip",
-    androidApiDemosLocal: "http://localhost:3000/ApiDemos-debug.apk"
+    androidGappiumApp: 'https://appium.s3.amazonaws.com/HelloGappium-android.apk'
   };
 } else {
   module.exports = {
-    iosTestApp: "sample-code/apps/TestApp/build/Release-iphonesimulator/TestApp.app",
-    iosWebviewApp: "sample-code/apps/WebViewApp/build/Release-iphonesimulator/WebViewApp.app",
-    iosUICatalogApp: "sample-code/apps/UICatalog/build/Release-iphonesimulator/UICatalog.app",
-    androidApiDemos: "sample-code/apps/ApiDemos/bin/ApiDemos-debug.apk",
-    selendroidTestApp: "sample-code/apps/selendroid-test-app.apk",
-    iosGappiumApp: '/Users/isaac/code/javascript-workshop/apps/HelloGappium.app.zip'
+    iosTestApp: path.resolve('apps/TestApp7.1.app.zip'),
+    androidContactManager: path.resolve('apps/ContactManager.apk'),
+    iosGappiumApp: path.resolve('apps/HelloGappium.app.zip'),
+    androidGappiumApp: path.resolve('apps/HelloGappium-android.apk')
   };
 }
--- tests/functional/helpers/caps.js
+++ tests/functional/helpers/caps.js
@@ -21,33 +21,21 @@ module.exports = {
   },
 
   android: {
-  }
-
-  // android18: {
-  //   browserName: '',
-  //   appiumVersion: '1.3.4',
-  //   platformName: 'Android',
-  //   platformVersion: '4.3',
-  //   deviceName: 'Android Emulator',
-  //   app: undefined // will be set later
-  // },
-
-  // android19: {
-  //   browserName: '',
-  //   appiumVersion: '1.3.4',
-  //   platformName: 'Android',
-  //   platformVersion: '4.4.2',
-  //   deviceName: 'Android Emulator',
-  //   app: undefined // will be set later
-  // },
-
-  // selendroid16: {
-  //   browserName: '',
-  //   appiumVersion: '1.3.4',
-  //   platformName: 'Android',
-  //   platformVersion: '4.1',
-  //   automationName: 'selendroid',
-  //   deviceName: 'Android Emulator',
-  //   app: undefined // will be set later
-  // }
+    '4.4': {
+      browserName: 'Browser',
+      appiumVersion: '1.3.4',
+      platformName: 'Android',
+      platformVersion: '4.4',
+      deviceName: 'Android Emulator',
+      app: undefined
+    },
+    '4.4c': {
+      browserName: 'Chrome',
+      appiumVersion: '1.3.4',
+      platformName: 'Android',
+      platformVersion: '4.4',
+      deviceName: 'Android Emulator',
+      app: undefined
+    }
+  },
 };
